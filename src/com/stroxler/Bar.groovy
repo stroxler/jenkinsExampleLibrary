c2af6dc3e41c16df68c17dd974a47a10457f4c42
Steven Troxler <steven.troxler@gmail.com>
2017-07-19 17:23:02 -0700

    Experiment with state

--- src/com/stroxler/Bar.groovy
+++ src/com/stroxler/Bar.groovy
@@ -1,21 +1,23 @@
 #!/usr/bin/env groovy
 package com.stroxler
 
+import java.lang.Thread
+
+class Globals {
+  public static int numTimesCalled = 0
+}
+
 def squareOfTwo() {
     return 2 * 2
 }
 
 
 def printSquareOfTwo() {
-    println("------------")
-    println("---YEAH-----")
-    println("---YEAH-----")
-    println("------------")
+    Globals.numTimesCalled += 1
     println("------------")
     println(squareOfTwo())
     println("------------")
-    println("---YEAH-----")
-    println("---YEAH-----")
+    println("called " + Globals.numTimesCalled + " times")
     println("------------")
-    println("------------")
+    Thread.sleep(10 * 1000)
 }
